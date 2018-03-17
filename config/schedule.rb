8e91a09aee760cee79e3e0b314e6a0f7dab5e4af
araluce <araluce11@gmail.com>
2018-03-17 13:55:51 +0100

    Crontab updated

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,3 +23,7 @@ set :environment, ENV['RAILS_ENV']
 every :weekday, at: '01:00' do
   rake 'cron:twitter', :output => {:error => 'log/twitter_cron_error.log', :standard => 'log/twitter_cron.log'}
 end
+
+every :monday, at: '01:00' do
+  rake 'cron:runtastic', :output => {:error => 'log/runtastic_cron_error.log', :standard => 'log/runtastic_cron.log'}
+end
