038333a8a994118259f8eea4a6571fd4f77d56c9
araluce <araluce11@gmail.com>
2018-03-21 20:07:42 +0100

    Mark message as viewed if clan

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
     @message = Message.new(object_params)
     @message.user = current_user
     @message.admin = current_admin
+    @message.viewed = true if @message.chatroom.is_admin?
 
     if @message.save
       # if current_user
@@ -26,19 +27,23 @@ class MessagesController < ApplicationController
     @user = User.find_by(id: params['user_id'].to_i)
     @admin = Admin.find_by(id: params['admin_id'].to_i)
     messages_to_ignore = params['ids'].flatten
-    chat = Chatroom.find_by(id: params['chat_id'].to_i)
-    if @admin.nil?
-      chat.messages.where(viewed: false).where.not(user_id: @user.id).update_all(viewed: true)
-    else
-      chat.messages.where(viewed: false).where.not(admin_id: @admin.id).update_all(viewed: true)
-    end
-    last_100_messages = chat.messages.order(created_at: :asc).last(100)
+    @chat = Chatroom.find_by(id: params['chat_id'].to_i)
+    mark_message_as_viewed
+    last_100_messages = @chat.messages.order(created_at: :asc).last(100)
     @messages = []
     last_100_messages.map {|message| @messages << message unless messages_to_ignore.include?(message.id.to_s)}
     render 'get_messages.coffee.js.erb'
   end
 
   private
+
+  def mark_message_as_viewed
+    if @admin.nil?
+      @chat.messages.where(viewed: false).where.not(user_id: @user.id).update_all(viewed: true)
+    else
+      @chat.messages.where(viewed: false).where.not(admin_id: @admin.id).update_all(viewed: true)
+    end
+  end
 
   def user_model
     User
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,16 @@
 class Chatroom < ApplicationRecord
   has_many :messages, inverse_of: :chatroom, dependent: :destroy
+
+  def is_clan?
+    type_to_s == 'Clan'
+  end
+
+  def is_individual?
+    type_to_s == 'Directo'
+  end
+
+  def is_admin?
+    type_to_s == 'Admin'
+  end
+
 end
