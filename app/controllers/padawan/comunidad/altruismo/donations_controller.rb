07abd9a5acc3a1a5b9c6e3c5ac6a03e05fabbb2c
araluce <araluce11@gmail.com>
2018-03-22 23:55:56 +0100

    Minor fixes in donations

--- app/controllers/padawan/comunidad/altruismo/donations_controller.rb
+++ app/controllers/padawan/comunidad/altruismo/donations_controller.rb
@@ -9,25 +9,35 @@ module Padawan
 
         def create
           @object = model.new(object_params)
-          set_seconds_donated
-          if @object.save
-            begin
-              PayService.user_pay_reason(@object.sender, @object.seconds_donated, @object.to_s)
-              PayService.system_pay_reason(@object.receiver, @object.seconds_donated, @object.to_s)
-              flash[:notice] = 'Donación realizada correctamente'
-              redirect_to action: :new
-            rescue
-              flash[:alert] = 'Se ha producido un error en el proceso de pago'
+
+          if user_permitted?(@object.receiver)
+            set_seconds_donated
+            if @object.save
+              begin
+                PayService.user_pay_reason(@object.sender, @object.seconds_donated, @object.to_s)
+                PayService.system_pay_reason(@object.receiver, @object.seconds_donated, @object.to_s)
+                flash[:notice] = 'Donación realizada correctamente'
+                redirect_to action: :new
+              rescue
+                flash[:alert] = 'Se ha producido un error en el proceso de pago'
+                render :new
+              end
+
+            else
+              @errors = @object.errors
               render :new
             end
-
           else
-            @errors = @object.errors
+            flash[:alert] = 'El padawan al que intentas donar tiene más de 7 días'
             render :new
           end
         end
 
         private
+
+        def user_permitted?(user)
+          (user.tdv - DateTime.now) >= 7
+        end
 
         def set_seconds_donated
           @object.seconds_donated = (@object.days.to_i.days + @object.hours.to_i.hours + @object.minutes.to_i.minutes + @object.seconds.to_i.seconds).to_i
