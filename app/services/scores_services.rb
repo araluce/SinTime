8f573db5ab261c81aef22bf943373c50f2401f16
araluce <araluce11@gmail.com>
2018-03-26 22:21:30 +0200

    Level service fixes

--- app/services/scores_services.rb
+++ app/services/scores_services.rb
@@ -2,7 +2,11 @@ class ScoresServices
   class << self
 
     def max_score
-      Score.find_by(title: 'Tú sí que vales')
+      [
+          Score.find_by(title: 'Tú sí que vales'),
+          Score.find_by(title: 'Rozando El Larguero'),
+          Score.find_by(title: 'Para Quitarse El Sombrero!'),
+      ]
     end
 
     def has_max_score_last_week?(user)
