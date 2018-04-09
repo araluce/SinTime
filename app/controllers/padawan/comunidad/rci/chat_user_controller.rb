1749e5da0cdc16cd995995906b36f73baa225be9
araluce <araluce11@gmail.com>
2018-04-09 22:21:16 +0200

    Mark messages as viewed

--- app/controllers/padawan/comunidad/rci/chat_user_controller.rb
+++ app/controllers/padawan/comunidad/rci/chat_user_controller.rb
@@ -8,11 +8,18 @@ module Padawan
           @chat = model.find_by(user_1_id: params[:id], user_2_id: current_user.id) if @chat.nil?
           @chat = model.create(user_1_id: current_user.id, user_2_id: params[:id]) if @chat.nil?
           @messages = @chat.messages.order(created_at: :asc).last(100)
+          mark_message_as_viewed
           @message = Message.new
         end
 
         def model
           Chat::Individual
+        end
+
+        private
+
+        def mark_message_as_viewed
+          @chat.messages.where(viewed: false).where.not(user_id: current_user.id).update_all(viewed: true)
         end
 
       end
