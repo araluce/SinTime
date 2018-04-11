85bd33f17ae2d1af5da8beaded894ba1115bec7b
araluce <araluce11@gmail.com>
2018-04-12 00:32:13 +0200

    Adding hangstag to follow

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,7 +8,7 @@ class StaticController < ApplicationController
   end
 
   def tweets
-    @sintime_tweets = TwitterService.get_latest_tweet_by_hashtags(['#ProyectoSinTime', '#proyectosintime'])
+    @sintime_tweets = TwitterService.get_latest_tweet_by_hashtags(['#ProyectoSinTime', '#SinTime_wARs'])
   end
 
   def register
