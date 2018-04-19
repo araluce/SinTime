d5897d9f23630c5ab60cd0ef8b19eb98547e54bc
araluce <araluce11@gmail.com>
2018-04-19 20:39:41 +0200

    Ranking in admin

--- app/controllers/manager/citizens_controller.rb
+++ app/controllers/manager/citizens_controller.rb
@@ -87,7 +87,7 @@ module Manager
     end
 
     def set_objects
-      @objects = model.order(email: :desc)
+      @objects = model.all.map{|user| {user: user, score: user.banking_movements.where(created_at: DateTime.now.beginning_of_month-1.month ..DateTime.now.end_of_month-1.month).map {|banking_movement| banking_movement.seconds_difference }.sum}}.sort_by{|obj| -obj[:score]}
     end
 
   end
