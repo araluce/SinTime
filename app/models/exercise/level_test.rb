d9272798512a8408a4a44ba8367ebcefb6ba860d
araluce <araluce11@gmail.com>
2018-04-23 22:49:28 +0200

    Show badge when new level test is available

--- app/models/exercise/level_test.rb
+++ app/models/exercise/level_test.rb
@@ -6,4 +6,22 @@ class Exercise::LevelTest < Exercise
   def to_s
     title
   end
+
+  def is_complete?
+    exercise_users.count < max_applicants ? false : true
+  end
+
+  class << self
+    def completed
+      all_completed = []
+      all.map {|level_test| all_completed << level_test if level_test.is_complete?}
+      all_completed
+    end
+
+    def uncompleted
+      all_uncompleted = []
+      all.map {|level_test| all_uncompleted << level_test unless level_test.is_complete?}
+      all_uncompleted
+    end
+  end
 end
